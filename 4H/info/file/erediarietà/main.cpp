f02a3cf82afb44ea2dde8d540631b0205c640a82
Gabri20021 <163420734+Gabri20021@users.noreply.github.com>
2025-01-30 10:59:16 +0000

    olajjjj

---
+++ 4H/info/file/erediarietà/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include "Rettangolo.h"
+#include "Quadrilatero.h"
+
+using namespace std;
+
+int main() {
+    
+    Quadrilatero E1(9,3,6,5);
+    Rettangolo R1(5,7);
+
+    cout << "[VALORI DEL QUADRILATERO]" << endl;
+    cout << "-------------------------------------" << endl;
+    E1.stampa();
+
+    cout << "[VALORI DEL RETTANGOLO]" << endl;
+    cout << "-------------------------------------" << endl;
+    R1.stampa();
+
+    return 0;
+}
